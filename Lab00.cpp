d7423a269aef3a8b3be102223876101dc1d00c2c
Adamacy <a.jakubiak.srem@gmail.com>
2025-03-05 19:44:04 +0100

    Initial commit

---
+++ Lab00.cpp
@@ -0,0 +1,9 @@
+// Lab00.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "I don't have Qt Creator on my PC. =( \n";
+}
